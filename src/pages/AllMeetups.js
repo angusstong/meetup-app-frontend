34cb63f964d56c030bdd8ccc0a9960ab0623c3c0
Angus Tong <angustongmanhin@gmail.com>
2021-04-29 12:19:27 +0800

    initial commit with frontend code

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  const api_url = process.env.REACT_APP_MEETUP_API;
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(api_url)
+      .then((response) => {
+        //console.log(response.json());
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,30 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+
+  const api_url = process.env.REACT_APP_MEETUP_API;
+
+  function addMeetupHandler(meetupData) {
+    fetch(api_url, {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
